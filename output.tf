b6fb344a0eb0642684f010210f2b000b1af75596
thaw <helloworld.22456@gmail.com>
2026-09-02 17:51:58 +0900

    added 2 public subnets and 1 private subnet

--- output.tf
+++ output.tf
@@ -1,4 +1,9 @@
-output "dev_vpc_id" {
-  value = aws_vpc.dev-VCP-tokyo.id
+output "dev_network_id" {
+  value = {
+    "vpc_id"              = aws_vpc.dev-VCP-tokyo.id,
+    "public_subnet_A_id"  = aws_subnet.dev-public-subnet-A.id,
+    "public_subnet_C_id"  = aws_subnet.dev-public-subnet-C.id,
+    "private_subnet_A_id" = aws_subnet.dev-private-subnet-A.id,
+  }
 
 }
